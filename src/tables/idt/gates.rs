5a92b28651e904cb3f3f6a16da3f0552f066fb54
Maxym Naumchyk <max.naumch@gmail.com>
2017-07-23 23:26:24 +0300

    New enum InterruptVector

--- src/tables/idt/gates.rs
+++ src/tables/idt/gates.rs
@@ -1,1 +1,28 @@
+/// The list of architecture defined interrupt vectors.
+/// For more information see Intel System Programming Guide.
+#[derive(Copy, Clone)]
+#[repr(u8)]
+pub enum InterruptVector {
 
+    DivideError     = 0,
+    DebugException  = 1,
+    Nmi             = 2,
+    Breakpoint      = 3,
+    Overflow        = 4,
+    BoundRange      = 5,
+    InvalidOpcode   = 6,
+    NoMath          = 7,
+    DoubleFault     = 8,
+
+    InvalidTss          = 10,
+    SegmentNotPresent   = 11,
+    StackSegmentFault   = 12,
+    GeneralProtection   = 13,
+    PageFault           = 14,
+
+    MathFault               = 16,
+    AlignmentCheck          = 17,
+    MachineCheck            = 18,
+    SimdException           = 19,
+    VirtualizationException = 20,
+}
