925bd7e3a9417f575e04df2f65853f76c2c314fe
Maxym Naumchyk <max.naumch@gmail.com>
2017-07-23 23:28:21 +0300

    Added TrapGate and InterruptGate structs and their Defaul trait impl

--- src/tables/idt/gates.rs
+++ src/tables/idt/gates.rs
@@ -26,3 +26,75 @@ pub enum InterruptVector {
     SimdException           = 19,
     VirtualizationException = 20,
 }
+
+/// The structure of the trap gate.
+#[repr(packed)]
+#[derive(Clone, Copy)]
+pub struct TrapGate {
+
+    /// First 16 bits of offset.
+    offset0     : u16,
+
+    /// Segment selector.
+    segsel      : u16,
+
+    flags       : u16,
+
+    /// Bits 16-31 of offset.
+    offset1     : u16,
+
+    /// Bits 32-63 of offset.
+    offset2     : u32,
+
+    _reserved   : u32,
+}
+
+/// The structure of the interrupt gate.
+#[repr(packed)]
+#[derive(Clone, Copy)]
+pub struct InterruptGate {
+
+    /// First 16 bits of offset.
+    offset0     : u16,
+
+    /// Segment selector.
+    segsel      : u16,
+
+    flags       : u16,
+
+    /// Bits 16-31 of offset.
+    offset1     : u16,
+
+    /// Bits 32-63 of offset.
+    offset2     : u32,
+
+    _reserved   : u32,
+}
+
+impl Default for TrapGate {
+
+    fn default() -> Self {
+        TrapGate {
+            offset0     : 0,
+            segsel      : 0,
+            flags       : (super::DescriptorType::TrapGate as u16) << 8,
+            offset1     : 0,
+            offset2     : 0,
+            _reserved   : 0,
+        }
+    }
+}
+
+impl Default for InterruptGate {
+
+    fn default() -> Self {
+        InterruptGate {
+            offset0     : 0,
+            segsel      : 0,
+            flags       : (super::DescriptorType::InterruptGate as u16) << 8,
+            offset1     : 0,
+            offset2     : 0,
+            _reserved   : 0,
+        }
+    }
+}
