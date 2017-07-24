01f0fcabf6cb7477163c9c6c1f5f94989011e2f9
Maxym Naumchyk <max.naumch@gmail.com>
2017-07-24 15:14:09 +0300

    New mod ent

--- src/tables/gdt/mod.rs
+++ src/tables/gdt/mod.rs
@@ -1,5 +1,9 @@
 use super::*;
 
+/// Module that implement different entries of GDT.
+mod ent;
+pub use self::ent::*;
+
 /// GDT controller. All operations on GDT are made through methods of this
 /// controller.
 pub struct GdtCtrl {
