67f5350e5821949d25ee3887902df6cbb7e25104
Maxym Naumchyk <max.naumch@gmail.com>
2017-11-18 14:33:49 +0200

    New struct Pic + impl

---
+++ src/pic.rs
@@ -0,0 +1,22 @@
+/// PIC interface.
+pub struct Pic {
+}
+
+impl Pic {
+
+    /// Disable PIC.
+    pub fn disable(&self) {
+        use port::Port;
+
+        let cmd = 0xFF; // Disable command
+        let slv = 0xA1; // Slave PIC data port.
+        let mst = 0x21; // Master PIC data port.
+
+        let slv = Port::from(slv as u16);
+        let mst = Port::from(mst as u16);
+
+        // Send disable commands.
+        slv.out_u8(cmd);
+        mst.out_u8(cmd);
+    }
+}
