5ea2ce2fdd66fa689def3f144d07ca887bcc730f
Maxym Naumchyk <max.naumch@gmail.com>
2017-11-21 00:18:12 +0200

    New fn enable

--- src/xsave.rs
+++ src/xsave.rs
@@ -2,3 +2,14 @@
 pub fn is_supported() -> bool {
     ::cpuid::Features::get().xsave_supported()
 }
+
+/// Enable XSAVE by modifying CR4.
+pub fn enable() {
+    unsafe {
+        use ::cr::Reg;
+
+        let mut cr4 = ::cr::Cr4::read();
+        cr4.enable_osxsave();
+        cr4.save();
+    }
+}
